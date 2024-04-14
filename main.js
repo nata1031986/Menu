4ee37c8ea0aa0956ac08268eff91a1a46483c625
Tasha Perry <nata1031986@gmail.com>
2024-04-14 16:07:06 +0300

    err

--- main.js
+++ main.js
@@ -61,73 +61,78 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/taco.png",
-    desc: ` Food truck truffaut Tortillas,Seasoned ground beef, grilled chicken, pork, shrimp, or fish, Lettuce or cabbage, Tomato, Onion, Cheese, Salsa, Sour cream, Guacamole, Lime, Cilantro (optional), Jalapeños (optional).`
+    desc: `   `
   },
 
 
 ];
-
 const sectionCenter = document.querySelector(".section-center");
-const container = document.querySelector(".btn-container");
-
-
-
-
-window.addEventListener("DOMContentLoaded", function() {
-   displayMenuItems(menu);
-   const categories = menu.reduce(function(values, item) {
-if (!values.includes(item.category)) {
-  values.push(item.category);
-}
-   return values;
-    
-   }, ["all"]);
- const categoryBtns = categories.map(function(category) {
-   return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
- })
- .join("");
- container.innerHTML = categoryBtns;
- const filterBtns = document.querySelectorAll(".filter-btn");
-  });
-
-
-
-
-filterBtns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
-const category = e.currentTarget.dataset.id;
-const menuCategory =menu.filter(function(menuItem){
-  if(menuItem.category === category) {
-    return menuItem;
-  }
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
 });
-  
-if(category === 'all') {
-  displayMenuItems(menu)
-}
-else {
-  displayMenuItems(menuCategory);
-}
-});
-});
-        
-function displayMenuItems(menuItems) {
- let displayMenu = menuItems.map(function (item) {
-        return `   <article class="menu-item">
-            <img src=${item.img} class="photo" alt=${item.title} />
-            <div class="item-info">
-                <header>
-                    <h4>${item.title}</h4>
-                    <h4 class="price">$${item.price}</h4>
-                </header>
-                <p class="item-text">${item.desc}</p>
-            </div>
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
         </article>`;
-    });
-    displayMenu = displayMenu.join("");
-    sectionCenter.innerHTML = displayMenu;
-}
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
 
-function displayMenuButtons () {
-  
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
 }
