1ce15f3c23e82a6fe24f029769eb654cea10cb34
Tasha Perry <nata1031986@gmail.com>
2024-04-14 15:48:07 +0300

    ee

--- main.js
+++ main.js
@@ -68,11 +68,49 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
+
+
+
 
 window.addEventListener("DOMContentLoaded", function() {
-   displayMenuItems(menu)
-});
+   displayMenuItems(menu);
+   const categories = menu.reduce(function(values, item) {
+if (!values.includes(item.category)) {
+  values.push(item.category);
+}
+   return values;
+    
+   }, ["all"]);
+ const categoryBtns = categories.map(function(category) {
+   return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+ })
+ .join("");
+ container.innerHTML = categoryBtns;
+ const filterBtns = document.querySelectorAll(".filter-btn");
+  });
 
+
+
+
+filterBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+const category = e.currentTarget.dataset.id;
+const menuCategory =menu.filter(function(menuItem){
+  if(menuItem.category === category) {
+    return menuItem;
+  }
+});
+  
+if(category === 'all') {
+  displayMenuItems(menu)
+}
+else {
+  displayMenuItems(menuCategory);
+}
+});
+});
+        
 function displayMenuItems(menuItems) {
  let displayMenu = menuItems.map(function (item) {
         return `   <article class="menu-item">
@@ -88,4 +126,8 @@ function displayMenuItems(menuItems) {
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons () {
+  
 }
